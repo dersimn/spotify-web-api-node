856b7c34ef7a59aa4a3d161fc2e077d22195234f
Simon Christmann <simon@christmann.email>
2020-08-13 21:44:18 +0200

    Implement automatic refresh of token

--- examples/00-get-access-token.js
+++ examples/00-get-access-token.js
@@ -61,8 +61,9 @@ app.get('/callback', (req, res) => {
     return;
   }
 
-  spotifyApi.authorizationCodeGrant(code).then(
-    data => {
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
       const access_token = data.body['access_token'];
       const refresh_token = data.body['refresh_token'];
       const expires_in = data.body['expires_in'];
@@ -74,21 +75,27 @@ app.get('/callback', (req, res) => {
       console.log('refresh_token:', refresh_token);
 
       console.log(
-        `Sucessfully retreived access token. Expires in ${
-          data.body['expires_in']
-        } s.`
+        `Sucessfully retreived access token. Expires in ${expires_in} s.`
       );
       res.send('Success! You can now close the window.');
-    },
-    error => {
+
+      setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
+        const access_token = data.body['access_token'];
+
+        console.log('The access token has been refreshed!');
+        console.log('access_token:', access_token);
+        spotifyApi.setAccessToken(access_token);
+      }, expires_in / 2 * 1000);
+    })
+    .catch(error => {
       console.error('Error getting Tokens:', error);
       res.send(`Error getting Tokens: ${error}`);
-    }
-  );
+    });
 });
 
-app.listen(8888, () => {
+app.listen(8888, () =>
   console.log(
     'HTTP Server up. Now go to http://localhost:8888/login in your browser.'
-  );
-});
+  )
+);
