dc5b1ae5b2a8464912395e9aad86cf83b12f9324
Simon Christmann <simon@christmann.email>
2020-08-13 21:44:24 +0200

    Add example

---
+++ examples/04-modify-playlists/01-add-tracks-to-playlist.js
@@ -0,0 +1,24 @@
+const SpotifyWebApi = require('../../');
+
+const spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
+
+(async () => {
+  const playlistArray = await spotifyApi.getAllUserPlaylists();
+  const playlistName = process.argv.slice(2)[0] || 'Test';
+  const testPlaylistId = playlistArray.find(p => p.name == playlistName).id;
+  console.log(`Using Playlist '${playlistName}' with id '${testPlaylistId}'`);
+
+  spotifyApi.addTracksToPlaylist(
+    testPlaylistId,
+    [
+      'spotify:track:4iV5W9uYEdYUVa79Axb7Rh',
+      'spotify:track:1301WleyT98MSxVHPZCA6M'
+    ],
+    {
+      position: 10
+    }
+  );
+})().catch(e => {
+  console.error(e);
+});
