b736be882ecb662e79de68fa30ddd70d93a5cab4
Simon Christmann <simon@christmann.email>
2020-11-27 19:42:07 +0100

    Fix missing body selector

--- examples/tutorial/01-basics/01-get-info-about-current-user.js
+++ examples/tutorial/01-basics/01-get-info-about-current-user.js
@@ -4,7 +4,7 @@ const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
 
 (async () => {
-  const me = await spotifyApi.getMe();
+  const me = (await spotifyApi.getMe()).body;
   console.log(me);
 })().catch(e => {
   console.error(e);
